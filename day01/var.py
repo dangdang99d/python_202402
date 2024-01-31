35750b5896ef4ebb91a180d203f9d770b38e5498
dangdang99d <158238165+dangdang99d@users.noreply.github.com>
2024-01-31 10:48:53 +0900

    Add files via upload

---
+++ day01/var.py
@@ -0,0 +1,16 @@
+#변수라는 뜻
+# myname = input("너의 이름은?")
+# age = 10 + 15
+# print(age)
+# print(myname)
+
+
+a = input("당신의 이름은?")
+b = input("당신의 나이?")
+c = input("당신의 최애 음식?")
+d = input("당신의 최애 유튜버?")
+print(a)
+print(b)
+print(c)
+print(d)
+
